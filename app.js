6313a6fa652d057fd8cb125134ae6a601feeb486
=Akhil-EM <=akhilemcalicut@gmail.com>
2022-08-29 23:16:56 +0530

    solved some error

--- app.js
+++ app.js
@@ -33,8 +33,9 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// // error handler
 app.use(function(err, req, res, next) {
+  console.log(err.message);
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -43,6 +44,9 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+   
 
 module.exports = app;
 
