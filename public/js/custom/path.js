50fdb47f637f926d4e865d583836af60a0a2bb3b
=Akhil-EM <=akhilemcalicut@gmail.com>
2022-08-30 10:27:53 +0530

    tracking display error in frontend

---
+++ public/js/custom/path.js
@@ -0,0 +1,6 @@
+let API_PATH = "http://ec2-54-175-243-156.compute-1.amazonaws.com:9090/api";
+
+let IMAGE_PATH = "https://movonnewbucket.s3.amazonaws.com/images/";
+let CAPTCHA_SITE_KEY = "6LeA17AgAAAAALRBrX_msKg5aD2dtH_UNTVxp4Kf";
+// let API_PATH = "http://localhost:9090/api";
+// let IMAGE_PATH = "http://localhost:9090/resources/images";
