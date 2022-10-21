c5ab02170d9edc492808e3413175dcb9cba1958b
Akhil Soman <akhil.s@TA.COM>
2022-10-21 20:39:27 +0530

    fade animation

--- public/js/custom/portfolios.js
+++ public/js/custom/portfolios.js
@@ -1,52 +1,57 @@
-async function getPortfolio(){
-    try{
-       let result = await axios.get(`${API_PATH}/util/frontend-token`);
-       let token = result.data.data.token;
-       let hexId = localStorage.getItem("hexId");
-       let portfolioImagesResult =  await axios.get(`${API_PATH}/portfolios/${hexId}/images/frontend`,{
-              headers: {"Authorization":"Bearer "+token}
-        }); 
-      
-        //let portfolios = portfolioResult.data.data.portfolios;
-        let portfolios = portfolioImagesResult.data.data.images;
+async function getPortfolio() {
+  try {
+    let result = await axios.get(`${API_PATH}/util/frontend-token`);
+    let token = result.data.data.token;
+    let hexId = localStorage.getItem("hexId");
+    let portfolioImagesResult = await axios.get(
+      `${API_PATH}/portfolios/${hexId}/images/frontend`,
+      {
+        headers: { Authorization: "Bearer " + token },
+      }
+    );
 
-                if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-  // true for mobile device
-            $('#albums_carousel').removeClass('is-large')
-            $('#albums_carousel').addClass('is-vertical');
-            
-}else{
-  // false for not mobile device
-            $('#albums_carousel').removeClass('is-vertical');
-            $('#albums_carousel').addClass('is-large');
-}
-        portfolios.forEach(portfolio => {
-            $('#albums_carousel').append(`<div class="ashade-album-item">
+    //let portfolios = portfolioResult.data.data.portfolios;
+    let portfolios = portfolioImagesResult.data.data.images;
+
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    ) {
+      // true for mobile device
+      $("#albums_carousel").removeClass("is-large");
+      $("#albums_carousel").addClass("is-vertical");
+    } else {
+      // false for not mobile device
+      $("#albums_carousel").removeClass("is-vertical");
+      $("#albums_carousel").addClass("is-large");
+    }
+    portfolios.forEach((portfolio) => {
+      $("#albums_carousel").append(`<div class="ashade-album-item fadein">
                                             <div class="ashade-album-item__inner">
                                                 <img style="object-fit: cover;" src="${IMAGE_PATH}${portfolio.image}" alt="${portfolio.image}" width="1000" height="1200">
                                                 <div class="ashade-album-item__overlay"></div>
                                             </div>
                                         </div>`);
-        });
-        
-        ashade_ribbon.init();
-         
-       //remove token 
-       axios.delete(`${API_PATH}/util/frontend-token`,{
-         headers: {"Authorization":"Bearer "+token}
-       })
-
-    }catch(error){
-       console.log(error);
-       alert("something went wrong")
+    });
+    var width = $(window).width();
+    if (width > 600) {
+      ashade_ribbon.init();
     }
+
+    //remove token
+    axios.delete(`${API_PATH}/util/frontend-token`, {
+      headers: { Authorization: "Bearer " + token },
+    });
+  } catch (error) {
+    console.log(error);
+    alert("something went wrong");
+  }
 }
 
 getPortfolio();
 
-
-function navigateToDetailPage(hexId){
-   
-   localStorage.setItem("hexId",hexId);
-   window.location.href= "/portfolio-detail";
+function navigateToDetailPage(hexId) {
+  localStorage.setItem("hexId", hexId);
+  window.location.href = "/portfolio-detail";
 }
